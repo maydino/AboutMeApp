9b1851b74b51521a2ae7858db6806b37be2a4ae5
maydino <mutluaydin.m@gmail.com>
2021-11-15 22:19:09 -0800

    Image view complated

--- AboutMeApp/AboutMeViewController.swift
+++ AboutMeApp/AboutMeViewController.swift
@@ -23,6 +23,27 @@ class AboutMeViewController: UIViewController {
         return image
     }()
     
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mutlu Aydin"
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let pronounceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "he/him/his"
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        label.textColor = .lightGray
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
@@ -34,6 +55,9 @@ class AboutMeViewController: UIViewController {
     func setUP () {
         view.addSubview(pictureView)
         pictureView.addSubview(picture)
+        pictureView.addSubview(nameLabel)
+        pictureView.addSubview(pronounceLabel)
+
 
         NSLayoutConstraint.activate([
             
@@ -45,7 +69,17 @@ class AboutMeViewController: UIViewController {
             picture.widthAnchor.constraint(equalToConstant: 200),
             picture.heightAnchor.constraint(equalToConstant: 200 ),
             picture.topAnchor.constraint(equalTo: pictureView.topAnchor, constant: 15),
-            picture.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
+            picture.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            
+            nameLabel.widthAnchor.constraint(equalToConstant: 150),
+            nameLabel.heightAnchor.constraint(equalToConstant: 25 ),
+            nameLabel.topAnchor.constraint(equalTo: picture.bottomAnchor, constant: 6),
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            
+            pronounceLabel.widthAnchor.constraint(equalToConstant: 150),
+            pronounceLabel.heightAnchor.constraint(equalToConstant: 25 ),
+            pronounceLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            pronounceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0)
         
         ])
     }
